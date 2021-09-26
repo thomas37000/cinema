17a6973eed29de111a4aa41728aa4e7aea6ff1fb
Thomas <thomas@avecnous.eu>
2021-09-26 17:36:51 +0200

    do some css for the cards home

--- src/components/CardMovies.tsx
+++ src/components/CardMovies.tsx
@@ -1,17 +1,19 @@
 import { IMovies } from '../MovieInterface';
-import Img_Api from "./Img_Api";
+import Img_Api from '../api/Img_Api';
 import './Card.css';
 
 const CardMovies = (props: { movie: IMovies }) => {
   const { movie } = props;
 
   return (
-    <div className='cardMovie'>
-      <div className='cardImage'>
-      <img src={Img_Api + movie.backdrop_path} alt={movie.title} />
+    <div className='allMovies'>
+      <div className='cardMovie'>
+        <div className='cardImg'>
+          <img src={Img_Api + movie.backdrop_path} alt={movie.title} />
+        </div>
+        <h2 className='title'>{movie.title}</h2>
+        {/* <p>{movie.overview}</p> */}
       </div>
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
     </div>
   );
 };
